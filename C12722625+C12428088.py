e82b63f92ff0fafad395acbbd24169251a65d0de
RoryHiggins <spaceoffz@gmail.com>
2016-04-06 19:43:08 +0100

    Enhancement to initial commit

--- C12722625+C12428088.py
+++ C12722625+C12428088.py
@@ -6,6 +6,7 @@ Authors:
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.feature_extraction import DictVectorizer
 from pprint import pprint
 
 headers = [
@@ -29,21 +30,30 @@ headers = [
     "feature"
 ]
 
-def main():
-    #with open("./data/trainingset.txt", "rb") as fl:
-    #    raw_data = fl.read()
-    #data = np.loadtxt(raw_data, delimiter=",")
-    data = pd.read_csv("data/trainingset.txt", index_col=0, names=headers)
-    
-    training_data = data.to_dict(orient="index")#[x: data[x] for x in training_headers]
-    training_feature = data["feature"]
-    
-    pprint(list(training_data.values())[:5])
-    #classifier = DecisionTreeClassifier(max_depth=3, criterion="entropy")
-    #classifier.fit(data.data, data.target)
-    
-    #export_graphviz(classifier.tree_, out_file='tree_d1.dot', feature_names=iris.feature_names)
 
+def get_training_data(data):
+    rows = data.to_dict(orient="index").values()
+    for row in rows:
+        del row["feature"]
+
+    vectorizer = DictVectorizer(sparse=False)
+    training_data = vectorizer.fit_transform(rows)
+
+    return training_data, vectorizer
+
+
+def main():
+    data = pd.read_csv("data/trainingset.txt", index_col=0, names=headers)
+
+    training_features = data["feature"]
+    training_data, vectorizer = get_training_data(data)
+
+    classifier = DecisionTreeClassifier(max_depth=3, criterion="entropy")
+    classifier.fit(training_data, training_features)
+
+    export_graphviz(classifier.tree_,
+                    out_file='tree_d1.dot',
+                    feature_names=training_features)
 
 if __name__ == "__main__":
     main()
