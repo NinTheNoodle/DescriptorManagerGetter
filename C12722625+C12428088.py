0d6e0dd433a44abfaa745f9ff3845cd528628991
Shane Farrell <docopoper@gmail.com>
2016-04-18 15:07:54 +0100

    Penultimate initial commit.

--- C12722625+C12428088.py
+++ C12722625+C12428088.py
@@ -31,6 +31,25 @@ headers = [
 ]
 
 
+
+
+def onehot_encode(dataframe):
+    result = {}
+    
+    for column, data_type in zip(dataframe, dataframe.dtypes):
+        cells = list(dataframe[column])
+        
+        if data_type == np.dtype("O"):
+            for category in set(cells):
+                new_column = "{}={}".format(column, category)
+                result[new_column] = [int(x == category) for x in cells]
+        elif data_type == np.dtype("int64"):
+            result[column] = cells
+        else:
+            raise NotImplemented("Unknown data type")
+
+    return result
+
 def get_training_data(data):
     rows = data.to_dict(orient="index").values()
     for row in rows:
@@ -46,14 +65,15 @@ def main():
     data = pd.read_csv("data/trainingset.txt", index_col=0, names=headers)
 
     training_features = data["feature"]
-    training_data, vectorizer = get_training_data(data)
+    pie = onehot_encode(data)
+    #training_data, vectorizer = get_training_data(data)
 
-    classifier = DecisionTreeClassifier(max_depth=3, criterion="entropy")
-    classifier.fit(training_data, training_features)
-
-    export_graphviz(classifier.tree_,
-                    out_file='tree_d1.dot',
-                    feature_names=training_features)
+    #classifier = DecisionTreeClassifier(max_depth=6, criterion="entropy")
+    #classifier.fit(training_data, training_features)
+    import pdb; pdb.set_trace()
+    #export_graphviz(classifier.tree_,
+    #                out_file='tree_d1.dot',
+    #                feature_names=training_features)
 
 if __name__ == "__main__":
     main()
