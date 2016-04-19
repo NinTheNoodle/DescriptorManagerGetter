c716809fa30738c46cd1663ab349283b80a7e0f8
RoryHiggins <spaceoffz@gmail.com>
2016-04-19 14:23:15 +0100

    Initial Commit 2: Electric Boogaloo

--- C12722625+C12428088.py
+++ C12722625+C12428088.py
@@ -3,11 +3,11 @@ Authors:
   Rory Higgins: C12428088
   Shane Farrell: C12722625
 """
-from math import log2
 from pprint import pprint
+from math import log2
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import export_graphviz
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction import DictVectorizer
 
@@ -33,17 +33,46 @@ names = [
 ]
 
 
-def main(fname="data/trainingset.txt", names=names):
+def dataframe_to_list(dataframe):
+    """
+    Converts dataframe to a list of dictionaries, and adds the "id" column
+
+    Unfortunately rather expensive, but necessary for DictVectorizer.
+    DataFrame.to_dict is not used as it does not preserve order
+    """
+    rows = []
+    for row_id, row in dataframe.iterrows():
+        row = dict(row)
+        row["id"] = row_id
+        rows.append(row)
+
+    return rows
+
+
+def drop_column(rows, column_name):
+    """
+    Extracts the values from a particular column and removes it from rows
+
+    The type of rows is expected to be a list of dictionaries
+    """
+    values = []
+    for row in rows:
+        values.append(row[column_name])
+        del row[column_name]
+
+    return values
+
+
+def main(data_fname="data/trainingset.txt",
+         query_fname="data/queries.txt",
+         names=names):
     # import pdb; pdb.set_trace()
 
     # extract data
-    data = pd.read_csv(fname, index_col=0, names=names)
-    rows = list(data.to_dict(orient="index").values())
-
-    # extract features
-    features = [row["feature"] for row in rows]
-    for row in rows:
-        del row["feature"]
+    data = pd.read_csv(data_fname, index_col=0, names=names)
+    rows = dataframe_to_list(data)
+    drop_column(rows, "id")
+    features = drop_column(rows, "feature")
 
     # perform one-hot encoding
     vectorizer = DictVectorizer(sparse=False)
@@ -56,6 +85,7 @@ def main(fname="data/trainingset.txt", names=names):
     validation_rows = rows[midway:]
     validation_features = features[midway:]
 
+    # generate random forest for data classification
     classifier = RandomForestClassifier(
         criterion="entropy",
         max_depth=int(log2(len(rows))) // 2,
@@ -64,8 +94,12 @@ def main(fname="data/trainingset.txt", names=names):
     classifier.fit(training_rows, training_features)
 
     accuracy = classifier.score(validation_rows, validation_features)
-    print(accuracy)
+    print("accuracy", accuracy)
 
+    # extract queries
+    query_data = pd.read_csv(query_fname, index_col=0, names=names)
+    query_ids = list(query_data.index)
+    queries = list(query_data.to_dict(orient="index").values())
 
 if __name__ == "__main__":
     main()
