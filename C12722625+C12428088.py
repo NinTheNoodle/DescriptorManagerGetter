a77b2b25e90d0ee5095d85408067bab6c0c46f31
RoryHiggins <spaceoffz@gmail.com>
2016-04-19 16:55:11 +0100

    commit.initialize()

--- C12722625+C12428088.py
+++ C12722625+C12428088.py
@@ -5,9 +5,10 @@ Authors:
 """
 from pprint import pprint
 from math import log2
+from random import shuffle
 import numpy as np
 import pandas as pd
-from sklearn.tree import export_graphviz
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction import DictVectorizer
 
@@ -68,14 +69,16 @@ def main(data_fname="data/trainingset.txt",
          names=names):
     # import pdb; pdb.set_trace()
 
+    vectorizer = DictVectorizer(sparse=False)
+
     # extract data
     data = pd.read_csv(data_fname, index_col=0, names=names)
     rows = to_list(data)
+    shuffle(rows)
+
     drop_column(rows, "id")
     features = drop_column(rows, "feature")
 
-    # perform one-hot encoding
-    vectorizer = DictVectorizer(sparse=False)
     rows = vectorizer.fit_transform(rows)
 
     # split into training and validation sets
@@ -87,20 +90,24 @@ def main(data_fname="data/trainingset.txt",
 
     # generate random forest for data classification
     classifier = RandomForestClassifier(
-        criterion="entropy",
-        max_depth=int(log2(len(rows))) // 2,
-        n_jobs=8
+        criterion="entropy"
     )
     classifier.fit(training_rows, training_features)
 
     accuracy = classifier.score(validation_rows, validation_features)
-    print("accuracy", accuracy)
 
     # extract queries
     query_data = pd.read_csv(query_fname, index_col=0, names=names)
     query_rows = to_list(query_data)
     query_ids = drop_column(query_rows, "id")
     drop_column(query_rows, "feature")
+    query_rows = vectorizer.fit_transform(query_rows)
+
+    result = classifier.predict(query_rows)
+
+    ratio = len([row for row in result if row == "TypeA"]) / len(result)
+    pprint("A/B ratio:", str(ratio))
+    pprint("accuracy:", accuracy)
 
 if __name__ == "__main__":
     main()
